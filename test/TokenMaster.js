7f3b52f453bc22173967684787bfd179eb3c38dd
arnoldmn <arnoldnzibuk@gmail.com>
2023-12-15 18:31:10 +0300

    code cleaning

--- test/TokenMaster.js
+++ test/TokenMaster.js
@@ -1,22 +1,28 @@
 const { expect } = require('chai')
 const { ethers } = require('hardhat')
 
+const NAME = "TokenMaster"
+const SYMBOL = "TM"
+
 describe("TokenMaster", () => {
     let tokenMaster
+    let deployer, buyer
 
     beforeEach(async () => {
+        [deployer, buyer] = await ethers.getSigners()
+        
         const TokenMaster = await ethers.getContractFactory("TokenMaster")
-        tokenMaster = await TokenMaster.deploy("TokenMaster", "TM")
+        tokenMaster = await TokenMaster.deploy(NAME, SYMBOL)
 
     })
 
     describe("Deployement", () => {
         it("Sets the name", async () => {
-            expect(await tokenMaster.name()).to.equal("TokenMaster")
+            expect(await tokenMaster.name()).to.equal(NAME)
         })
 
         it("Sets the symbol", async () => {
-            expect(await tokenMaster.symbol()).to.equal("TM")
+            expect(await tokenMaster.symbol()).to.equal(SYMBOL)
 
         })
     })
