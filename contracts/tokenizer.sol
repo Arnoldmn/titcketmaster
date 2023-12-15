321629d7dbdaa347ee4393b02b689f8f16aa6c82
arnoldmn <arnoldnzibuk@gmail.com>
2023-12-15 16:15:00 +0300

    Fix openzeppelin error

--- contracts/tokenizer.sol
+++ contracts/tokenizer.sol
@@ -1,14 +1,14 @@
 // SPDX-License-Identifer: MIT
 pragma solidity ^0.8.19;
 
-import "@openzeppelin/contracts/token/ERC721.sol";
+import  "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract TokenMaster is ERC721 {
 
     constructor(
         string _name, 
         string memory _symbol) ERC721(_name, _symbol) {
-
-            
+ 
+        
     }
 }
